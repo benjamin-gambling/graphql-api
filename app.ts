458b5e90d9e12160012a9edabdfdfb3e93e7d6fd
Benjamin Gambling <benjamin.gambling@gmail.com>
2020-09-18 09:38:21 -0700

    init commit

---
+++ app.ts
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolvers = require("./graphql/resolvers");
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers,
+    graphiql: true,
+  })
+);
+
+const uri: string = `${process.env.MONGODB_URI}`;
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+mongoose
+  .connect(uri, options)
+  .then(() => app.listen(3000, console.log("Server is running")))
+  .catch((error: any) => {
+    throw error;
+  });
